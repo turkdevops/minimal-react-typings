c4937384e4b1c35fd3f37bb621743be0bc282662
Kofi Ohene-Adu <kofi@clever.com>
2017-09-15 14:13:34 -0500

    Flesh out typings a little more. (#3)
    * Flesh out typings a little more.
    
    Added support for a few more commonly used properties - copied over from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/index.d.ts
    
    - Add `React.[createElement, Children | PureComponent | PropTypes]`
    - Add support for optional `context` argument in constructor.
    - Better define `setState` to include the optional callback.
    - Also removing pre-defined static properties (like propTypes and defaultProps) so clients have to explicitly define them in the component class, encouraging the use of static typing.
    
    * 2.0.0

--- index.d.ts
+++ index.d.ts
@@ -6,14 +6,72 @@ export = React;
 export as namespace React;
 
 declare namespace React {
+  type Props = {[key: string]: any};
+  type State = Props;
+  type Context = Props;
+
   export class Component {
-    state: any;
-    props: any;
-    setState: Function;
-    constructor(props: any);
-    static propTypes: any;
-    static defaultProps: any;
+    context: Context;
+    props: Props;
+    state: State;
+    setState: (partialState: State, cb?: Function) => void;
+    constructor(props: Props, context?: Context);
   }
+
+  export class PureComponent extends Component {}
+
   function createElement(...args:any[]): any;
-  var PropTypes : any;
+
+  var Children: ReactChildren;
+  var PropTypes: ReactPropTypes;
+
+
+  //
+  // Original @types/react definitions:
+  // Copied from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/index.d.ts
+  //
+
+  //
+  // React.PropTypes
+  // ----------------------------------------------------------------------
+
+  interface Validator<T> {
+      (object: T, key: string, componentName: string, ...rest: any[]): Error | null;
+  }
+
+  interface Requireable<T> extends Validator<T> {
+      isRequired: Validator<T>;
+  }
+
+  type ValidationMap<T> = {[K in keyof T]?: Validator<T>};
+
+  interface ReactPropTypes {
+      any: Requireable<any>;
+      array: Requireable<any>;
+      arrayOf(type: Validator<any>): Requireable<any>;
+      bool: Requireable<any>;
+      element: Requireable<any>;
+      func: Requireable<any>;
+      instanceOf(expectedClass: {}): Requireable<any>;
+      node: Requireable<any>;
+      number: Requireable<any>;
+      object: Requireable<any>;
+      objectOf(type: Validator<any>): Requireable<any>;
+      oneOf(types: any[]): Requireable<any>;
+      oneOfType(types: Validator<any>[]): Requireable<any>;
+      shape(type: ValidationMap<any>): Requireable<any>;
+      string: Requireable<any>;
+  }
+
+  //
+  // React.Children
+  // ----------------------------------------------------------------------
+
+  interface ReactChildren {
+    map<T>(children: any, fn: (child: any, index: number) => T): T[];
+    forEach(children: any, fn: (child: any, index: number) => any): void;
+    count(children: any): number;
+    only(children: any): any;
+    toArray(children: any): any[];
+  }
 }
