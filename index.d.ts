57f793e4bf92b565a82ad9723bc06210c34d28c1
Cosmo Wolfe <cosmo.wolfe@gmail.com>
2018-04-24 12:28:28 -0700

    Update for react 16

--- index.d.ts
+++ index.d.ts
@@ -24,49 +24,11 @@ declare namespace React {
   export class PureComponent extends Component {}
 
   function createElement(...args:any[]): any;
+  function createContext(...args:any[]): any;
   function cloneElement(element: any, props?: Props, ...children: any[]): any
 
   var Children: ReactChildren;
   const Fragment: any;
-  var PropTypes: ReactPropTypes;
-
-
-  //
-  // Original @types/react definitions:
-  // Copied from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/index.d.ts
-  //
-
-  //
-  // React.PropTypes
-  // ----------------------------------------------------------------------
-
-  interface Validator<T> {
-      (object: T, key: string, componentName: string, ...rest: any[]): Error | null;
-  }
-
-  interface Requireable<T> extends Validator<T> {
-      isRequired: Validator<T>;
-  }
-
-  type ValidationMap<T> = {[K in keyof T]?: Validator<T>};
-
-  interface ReactPropTypes {
-      any: Requireable<any>;
-      array: Requireable<any>;
-      arrayOf(type: Validator<any>): Requireable<any>;
-      bool: Requireable<any>;
-      element: Requireable<any>;
-      func: Requireable<any>;
-      instanceOf(expectedClass: {}): Requireable<any>;
-      node: Requireable<any>;
-      number: Requireable<any>;
-      object: Requireable<any>;
-      objectOf(type: Validator<any>): Requireable<any>;
-      oneOf(types: any[]): Requireable<any>;
-      oneOfType(types: Validator<any>[]): Requireable<any>;
-      shape(type: ValidationMap<any>): Requireable<any>;
-      string: Requireable<any>;
-  }
 
   //
   // React.Children
